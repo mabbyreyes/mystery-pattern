7821b8c46116dd5650cf7a680827d4ec5082b21a
mabbyreyes <mabbyreyes@gmail.com>
2020-01-21 16:38:48 -0700

    Initial commit

---
+++ app/src/main/java/edu/com/deepdive/mysterypattern/controller/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.com.deepdive.mysterypattern.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import edu.com.deepdive.mysterypattern.R;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
